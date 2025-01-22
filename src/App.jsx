c2aab8e30a6ed5cd6e8d76c2ed23a36ffb4f5cf3
Dinu-Sreekumar <dinusrewkumar@gmail.com>
2025-01-22 15:41:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Dinu Sreekumar</h1>
+      <input type="text" placeholder='username' />
+    </>
+  )
+}
+
+export default App
